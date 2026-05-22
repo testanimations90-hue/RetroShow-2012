de89e62fb20bb4df862035ec5d22b69a17ea4298
testanimations90-hue <testanimations90@gmail.com>
2026-05-22 12:27:07 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('RETROSHOW_DB_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'retroshow.sqlite');
 define('RETROSHOW_DB_DSN', 'sqlite:' . RETROSHOW_DB_PATH);
 define('RETROSHOW_ADMINS', serialize([
-    'BitByByte'
+    'ADMIN'
 ]));
 
 define('RETROSHOW_PROCESSING_SERVER', 'http://127.0.0.1:8090');
@@ -11,11 +11,10 @@ define('RETROSHOW_PROCESSING_SERVER', 'http://127.0.0.1:8090');
 // Настройка Mailer.
 // -------------------------------------------------------------------------------------------------
 
-define('SMTP_HOST', 'w10.host');
-define('SMTP_PORT', 2525);
+define('SMTP_HOST', 'example.com');
+define('SMTP_PORT', 25);
 define('SMTP_SECURE', 'none'); // 'ssl', 'tls', 'none'
-define('SMTP_USERNAME', 'bitbybyte@w10.site');
-define('SMTP_PASSWORD', 'dfh3HKVdkr4ef3PEPf');
-define('SMTP_FROM_EMAIL', 'bitbybyte@w10.site');
+define('SMTP_USERNAME', 'domain@example.com');
+define('SMTP_PASSWORD', 'password');
+define('SMTP_FROM_EMAIL', 'domain@example.com');
 define('SMTP_FROM_NAME', 'RetroShow');
-
